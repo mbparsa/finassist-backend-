1f444083052f0a4aa8155e71de4dff02b039dc22
MB Parsa <mbparsa@mac.lan>
2024-11-06 21:49:53 -0800

    Initial setup

---
+++ database.py
@@ -0,0 +1,25 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import os
+
+
+user_name = os.environ.get('DB_USERNAME')
+password = os.environ.get('DB_PASSWORD')
+
+print(user_name)
+DATABASE_URL = f"postgresql://{user_name}:{password}!@localhost/finassist"
+print(DATABASE_URL)
+
+# Set up the database engine
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
---
+++ main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+import models, schemas, crud
+from database import engine, Base, get_db
+
+# Create all tables in the database
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+@app.post("/users/", response_model=schemas.User)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = db.query(models.User).filter(models.User.email == user.email).first()
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    return crud.create_user(db=db, user=user)
+
+@app.get("/users/", response_model=list[schemas.User])
+def read_users(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
+    users = crud.get_users(db, skip=skip, limit=limit)
+    return users
